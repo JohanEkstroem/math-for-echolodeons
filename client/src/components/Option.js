e4dd1064ad32c51b8a80e560b4660add84faf0b4
JohanEkstroem <johan@ekstrem@live.se>
2023-03-24 21:28:11 +0100

    Replace class name for colored table

--- client/src/components/Option.js
+++ client/src/components/Option.js
@@ -9,12 +9,12 @@ const Option = ({ table, toggleActive }) => {
 				<h1>Matte för minimonster</h1>
 				<Row >
 					{table.slice(0, 5).map((element, index) => (
-						<Col className={element.isActive ? "col p-2 m-2 rounded active" : "col p-2 m-2 rounded inactive"} key={index} onClick={() => toggleActive(element.value)}>{element.value}</Col>
+						<Col className={element.isActive ? "col p-2 m-2 rounded btn btn-success" : "col p-2 m-2 rounded btn btn-secondary"} key={index} onClick={() => toggleActive(element.value)}>{element.value}</Col>
 					))}
 				</Row>
 				<Row>
 					{table.slice(5, 10).map((element, index) => (
-						<Col className={element.isActive ? "col p-2 m-2 rounded active" : "col p-2 m-2 rounded inactive"} key={index} onClick={() => toggleActive(element.value)}>{element.value}</Col>
+						<Col className={element.isActive ? "col p-2 m-2 rounded btn btn-success" : "col p-2 m-2 rounded btn btn-secondary"} key={index} onClick={() => toggleActive(element.value)}>{element.value}</Col>
 					))}
 				</Row>
 			</div>
